3e9f888807e872765a04d727dc876e1779d9d5b7
mohzobeidah <mohzobeidah@gmail.com>
2022-07-19 13:42:40 +0300

    1

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors( this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    //We are using basic CORS policy settings because allowing any origin,
+                    //method, and header is okay for now.But we should be more
+                    //restrictive with those settings in the production environment.More
+                    //precisely, as restrictive as possible.
+                    //Instead of the AllowAnyOrigin() method which allows requests from any
+                    //source, we can use the WithOrigins("https://example.com") which will
+                    //allow requests only from that concrete source.Also, instead of9
+                    //AllowAnyMethod() that allows all HTTP methods, we can use
+                    //WithMethods("POST", "GET") that will allow only specific HTTP methods.
+                    //Furthermore, you can make the same changes for the AllowAnyHeader()
+                    //method by using, for example, the WithHeaders("accept", "contenttype") method to allow only specific headers.
+                    builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+
+                });
+            });
+        }
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+        });
+    }
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,71 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+//app.Use(async (context, next) =>
+//{
+//    Console.WriteLine($"Logic before executing the next delegate in the Use method \n");
+//    await context.Response.WriteAsync("Hello from the middleware component.");
+//    await next.Invoke();
+//    Console.WriteLine($"Logic after executing the next delegate in the Use method \n");
+//});
+//app.Map("/usingmapbranch", builder =>
+//{
+//    builder.Use(async (context, next) =>
+//    {
+//        Console.WriteLine(@"Map branch logic in the Use method before the next
+//        delegate");
+//        await next.Invoke();
+//        Console.WriteLine(@"Map branch logic in the Use method after the next
+//        delegate");
+//    });
+//    builder.Run(async context =>
+//    {
+//        Console.WriteLine($"Map branch response to the client in the Run method");
+//        await context.Response.WriteAsync("Hello from the map branch.");
+//    });
+//});
+//app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder
+//=>
+//{
+//    builder.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Hello from the MapWhen branch.");
+//    });
+//});
+//app.Run(async context =>
+//{
+//    Console.WriteLine($"Writing the response to the client in the Run method \n");
+//  //  context.Response.StatusCode = 200;
+//    await context.Response.WriteAsync("Hello from the middleware component. \n");
+//});
+app.MapControllers();
+
+app.Run();
+
